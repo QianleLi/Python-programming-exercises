c7ce94dec5c9d699145aef182b61901e11233533
Qianle Li <qli59@stevens.com>
2020-01-06 09:21:45 +0800

    Q5 submit

---
+++ program/Q5.py
@@ -0,0 +1,34 @@
+'''
+Question:
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+'''
+class mission:
+    def getString(self):
+        String = input('Please input a message:')
+        return String
+    def printString(self,String):
+        print(String.upper())
+
+m = mission()
+String = m.getString()
+m.printString(String)
+
+'''
+Solution:
+class InputOutString(object):
+    def __init__(self):
+        self.s = ""
+
+    def getString(self):
+        self.s = raw_input()
+
+    def printString(self):
+        print self.s.upper()
+
+strObj = InputOutString()
+strObj.getString()
+strObj.printString()
+'''
